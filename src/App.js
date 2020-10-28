1c1d274294b880d2d9efc3f95d47331ce4205a99
Waleed Jameel Hyderi <waleedj1699@gmail.com>
2020-10-28 10:07:52 +0530

    added random word api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import WordBinder from "./Components/WordBinder";
 import SearchIcon from "@material-ui/icons/Search";
 import "./styles.css";
@@ -6,12 +6,26 @@ import "./styles.css";
 export default function App() {
   const [inputWord, setWord] = useState("");
   const [searchWord, setSearchWord] = useState("Nice");
-
+  const RandWordApi = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=mhvfzxrhb41uag20wv1ycfl20yxq6f5zabdt82eutc2wuf1qf"
+  const [randWord,setRandDay] = useState("run");
+  
+  async function fetchRandWord() {
+    const res = await fetch(RandWordApi);
+    res
+      .json()
+      .then((res) => {
+        setRandDay(res[0].word)
+      })
+      .catch((err) => console.log(err));
+  }
+  useEffect(()=>{
+    fetchRandWord();
+  },[])
   return (
     <div className="App">
       <div className="word-day">
         <h2>Word of The Day : </h2>
-        <WordBinder searchedWord="run" limit="5" />
+        <WordBinder searchedWord={randWord} limit="5" />
       </div>
       <label className="sub-container input-style">
         <input
